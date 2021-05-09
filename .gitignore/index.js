ecaf5896e2b2e531bf1f83ae366cd4fe1b52a26f
KiriSunoKyuta <83513655+KiriSunoKyuta@users.noreply.github.com>
2021-05-09 21:32:17 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on("ready", async () => {
+    console.log("Bot Online");
+    bot.user.setStatus("online");
+    setTimeout(() => {
+        bot.user.setActivity("SyprineMaster", {type: 'WATCHING'} );
+    }, 100)
+});
+
+bot.on("guildMemberAdd", member => {
+    member.send(`Yoo, bienvenue sur le serveur ! Va lire le règlement et clique sur la petite emote verte en dessous pour avoir accès au serveur ! 
+Hâte de te voir parmi nous ${member}!`);
+})
+
+
+
+client.login(process.env.TOKEN)
